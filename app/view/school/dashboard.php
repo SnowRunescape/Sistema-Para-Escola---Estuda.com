ec42432e352e9289ec3e3f2f2268ad15cb03de78
SnowRunescape <snowrunescape@snowdev.com.br>
2021-02-09 10:25:12 -0400

    moving the css from the dashboard to the css file

--- app/view/school/dashboard.php
+++ app/view/school/dashboard.php
@@ -29,34 +29,6 @@
 				
 				<div class="divisory"></div>
 				
-				<style>
-					#dashboard-info {
-						padding:24px 0 12px !important;
-					}
-					
-					#dashboard-info .info {
-						padding: 0 24px;
-						display: inline-block;
-						text-align:center;
-					}
-					
-					#dashboard-info .info p:nth-child(2){
-						margin-top:2px;
-						font-size:33px;
-						color:#0878dd;
-					}
-					
-					#dashboard-info .info p:nth-child(1){
-						font-size:21px;
-						font-weight:600;
-						color:#333;
-					}
-					
-					#dashboard-info .info:not(:first-child){
-						border-left: solid 2px #f0eff6;
-					}
-				</style>
-				
 				<div id="dashboard-info" class="content">
 					<div class="info">
 						<p>Numero de Alunos</p>
