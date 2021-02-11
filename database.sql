89bb731b615c6d128a41036ae16ea84da0258083
SnowRunescape <snowrunescape@snowdev.com.br>
2021-02-11 10:08:12 -0400

    new release

--- database.sql
+++ database.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.5
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Feb 08, 2021 at 02:23 AM
--- Server version: 10.4.15-MariaDB
--- PHP Version: 7.2.34
+-- Host: localhost
+-- Generation Time: Feb 11, 2021 at 02:07 PM
+-- Server version: 5.7.25
+-- PHP Version: 7.1.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `u402551324_estuda`
+-- Database: `estuda`
 --
 
 -- --------------------------------------------------------
@@ -35,6 +35,18 @@ CREATE TABLE `classes` (
   `series` int(1) NOT NULL,
   `period` int(1) NOT NULL,
   `year` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `classes_students`
+--
+
+CREATE TABLE `classes_students` (
+  `id` int(11) NOT NULL,
+  `student` int(11) NOT NULL,
+  `class` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -63,7 +75,8 @@ CREATE TABLE `students` (
   `email` varchar(64) NOT NULL,
   `phone` varchar(16) NOT NULL,
   `birthday` date NOT NULL,
-  `genre` int(1) NOT NULL
+  `genre` int(1) NOT NULL,
+  `school_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -74,6 +87,12 @@ CREATE TABLE `students` (
 -- Indexes for table `classes`
 --
 ALTER TABLE `classes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `classes_students`
+--
+ALTER TABLE `classes_students`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -96,6 +115,12 @@ ALTER TABLE `students`
 -- AUTO_INCREMENT for table `classes`
 --
 ALTER TABLE `classes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `classes_students`
+--
+ALTER TABLE `classes_students`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
